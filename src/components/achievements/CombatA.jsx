4d20c5d37c383ba5ab30ebd1118bc0f75fd4ed82
snaramirez872 <seanramirez818@gmail.com>
2024-05-24 12:44:44 -0400

    fixed combat sections and added companion achievements

--- src/components/achievements/CombatA.jsx
+++ src/components/achievements/CombatA.jsx
@@ -18,23 +18,58 @@ function CombatA() {
             <Link to='/achievement-guides'>
                 <p>Back</p>
             </Link>
-            <section className='combat-intro'>
+            <section className='sec-intro'>
                 <p>The Wasteland featured in the Fallout universe plays host to many NPCs and Irradiated Creatures. Some of which start fights with the player. You can also start the fights with them, but do so carefully.</p>
             </section>
-            <section className='combat'>
+            <section className='main'>
                 <h1>Combat Milestones</h1>
                 <Table dat={combat} />
                 <Disclaimer />
             </section>
-            <section className='extra-combat-info'>
+            <section className='extra-info'>
                 <h1>Helpful Information</h1>
                 <ul>
                     <li>Combat statistics and other helpful gameplay information can be found in your Pip-Boy by going to <b>DATA</b> then to <b>STATS</b>.</li>
                     <li><b>"Giant Creatures"</b> refers to the likes of Mirelurk Queens and Super Mutant Behemoths.</li>
                     <ul>
-                        <li>A list of spawn locations can be found <a href='https://fallout.fandom.com/wiki/...The_Harder_They_Fall' target='_blank' rel='noreferrer'>here</a>.</li>
+                        <li>Spawn Locations</li>
+                        <ul>
+                            <li>Mirelurk Queens</li>
+                            <ul>
+                                <li><span className='location'>The Castle</span> as part of the quest <span className='quest'>"Taking Independence"</span> (The Castle can be visited and cleared by the player prior to this quest)</li>
+                                <li><span className='location'>Murkwater Construction Site</span> (Must be defeated to claim the settlement)</li>
+                                <li><span className='location'>Spectacle Island</span> (Spawns once the circuit breaker in the southern boat is turned on)</li>
+                                <li><span className='location'>Nahant Chapel</span> (near the coast)</li>
+                                <li><span className='location'>Massachussetts State House</span> (basement)</li>
+                                <li>South of <span className='location'>Warwick Homestead</span>, East of <span className='location'>Poseidon Energy Plant</span> (close to a capsized boat)</li>
+                                <li>West of <span className='location'>Salem</span>, Northwest of <span className='location'>Museum of Witchcraft</span></li>
+                                <li>Southeast of <span className='location'>Revere Beach Station</span> (near the beach)</li>
+                                <li>Southeast of <span className='location'>Recon Bunker Theta</span></li>
+                                <li><b>[Far Harbor]</b> <span className='location'>Emmet's Causeway</span> during the quest <span className='quest'>"Rite of Passage"</span></li>
+                                <li><b>[Far Harbor]</b> <span classNAme='location'>Northwood Ridge Quarry</span></li>
+                                <li><b>[Vault-Tec Workshop]</b> <span className='location'>Vault 88</span> in the water by the water pump (Spawns after taking out a group of mirelurks)</li>
+                                <li><b>[Nuka World]</b> in the lake by <span className='locaion'>Northpoint Reservoir</span></li>
+                            </ul>
+                            <li>Super Mutant Behemoths</li>
+                            <ul>
+                                <li>Outside of <span className='location'>Fort Strong</span></li>
+                                <li>West of <span className='location'>Natick Banks</span></li>
+                                <li>In the woods South of <span className='location'>Greentop Nursery</span> and East of <span className='location'>Malden Middle School</span></li>
+                                <ul>
+                                    <li>Will only spawn once a Jangles the Moon Monkey toy is looted out of a car that crashed into a nearby tree. Will <u>NOT</u> spawn if you order a companion to grab it</li>
+                                </ul>
+                                <li>West of <span className='location'>Suffolk County Charter School</span></li>
+                                <li>South of <span className='location'>Walden Pond</span> (Spawns the Stonehenge made of cars)</li>
+                                <li>A uniquely named Behemoth named "Swan" can be found at <span className='location'>Swan's Pond</span> at <span className='location'>Boston Common</span></li>
+                                <li>South of <span className='location'>Recon Bunker Theta</span></li>
+                                <li>Random spawn in front of the <span className='location'>Super Duper Mart</span> in <span className='location'>Lexington</span></li>
+                                <li><b>[Far Harbor]</b> A uniquel named Behemoth named "Grun" can be found behind a red bay door that can be unlcoked via nearby terminal inside the <span classNam='location'>Vim! Pop Factory</span></li>
+                                <li><b>[Nuka World]</b> In a small gulley Northwest of <span className='location'> Dry Rock Gulch</span></li>
+                            </ul>
+                        </ul>
                     </ul>
                     <li>The player <b>MUST</b> be the one to kill the NPC, Animal, and Giant Creature for it to count.</li>
+                    <li>A Super Mutant Behemoth spawns on the way to the <span className='location'>C.I.T. Ruins</span> during the quest <span className='quest'>"Ad Victoriam"</span>, but it is shortly killed by Liberty Prime.</li>
                 </ul>
             </section>
         </div>
--- src/components/achievements/CompanionA.jsx
+++ src/components/achievements/CompanionA.jsx
@@ -1,14 +1,32 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import Table from "../reusable/Table";
+import Disclaimer from "../reusable/Disclaimer";
+
+import Companion_001 from '../../assets/images/achievement-icons/companion_001.jpg';
+import Companion_002 from '../../assets/images/achievement-icons/companion_002.jpg';
 
 function CompanionA() {
+    const companion = [
+        [Companion_001,'Never Go It Alone', 'Recruit 5 separate companions', '20G', 'Bronze'],
+        [Companion_002,'Lovable', 'Reach maximum relationship level with a companion', '20G', 'Bronze']
+    ];
     return (
         <div className='achieve-home'>
             <Link to='/achievement-guides'>
                 <p>Back</p>
             </Link>
-            <section className='companions'>
+            <section className='sec-intro'>
+                <p>Like the other Fallout games, Fallout 4 offers a companion system. Each companion has a unique personality and they gain or lose affinity based on the player's actions and responses in dialogue. This takes the place of the Karma system that was present in previous games.</p>
+            </section>
+            <section className='main'>
                 <h1>Companion Milestones</h1>
+                <Table dat={companion} />
+                <Disclaimer />
+            </section>
+            <section className='extra-info'>
+                <h1>Helpful Information</h1>
+                <p>More information about the different companions can be found <a href='#' target='_blank' rel='noreferrer'>here</a>.</p>
             </section>
         </div>
     );
