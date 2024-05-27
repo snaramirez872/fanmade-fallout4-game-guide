a4abd5fbc9ad6600998917581a367aa24708be08
snaramirez872 <seanramirez818@gmail.com>
2024-05-27 19:54:23 -0400

    started work on dlc's

--- src/components/achievements/DLC/Automatron.jsx
+++ src/components/achievements/DLC/Automatron.jsx
@@ -3,18 +3,28 @@ import { Link } from "react-router-dom";
 import Table from "../../reusable/Table";
 import Disclaimer from "../../reusable/Disclaimer";
 
+import Automatron_001 from '../../assets/images/achievement-icons/automatron_001.jpg';
+import Automatron_002 from '../../assets/images/achievement-icons/automatron_002.jpg';
+import Automatron_003 from '../../assets/images/achievement-icons/automatron_003.jpg';
+import Automatron_004 from '../../assets/images/achievement-icons/automatron_004.jpg';
+import Automatron_005 from '../../assets/images/achievement-icons/automatron_005.jpg';
+
 function Automatron() {
-    const automatron = [];
+    const automatron = [
+        [Automatron_001, 'Mechanical Menace', 'Complete the quest \"Mechanical Menace\"', '20G', 'Bronze'],
+        [Automatron_002, 'Headhunting', 'Complete the quest \"Headhunting\"', '20G', 'Bronze'],
+        [Automatron_003, 'Restarting Order', 'Complete the quest \"Restoring Order\"', '20G', 'Bronze'],
+        [Automatron_004, 'Robot Hunter', 'Unlock 10 Robot Mods', '20G', 'Bronze'],
+        [Automatron_005, 'The Most Toys', 'Build 10 Robot Mods', '20G', 'Bronze']
+    ];
 
     return (
         <div className='automatron'>
             <Link to='/achievement-guides'>
                 <p>Back</p>
             </Link>
-            <section className='sec-intro'>
-                <p>[Replace with Text]</p>
-            </section>
             <section className='main'>
+                <h1>Automatron</h1>
                 <Table dat={automatron} />
                 <Disclaimer />
             </section>
--- src/components/achievements/DLC/WW.jsx
+++ src/components/achievements/DLC/WW.jsx
@@ -3,20 +3,41 @@ import { Link } from "react-router-dom";
 import Table from "../../reusable/Table";
 import Disclaimer from "../../reusable/Disclaimer";
 
+import ww_001 from '../../assets/images/achievement-icons/ww_001.jpg';
+import ww_002 from '../../assets/images/achievement-icons/ww_002.jpg';
+import ww_003 from '../../assets/images/achievement-icons/ww_003.jpg';
+
 function WastelandWorkshop() {
-    const wasteland = [];
+    const wasteland = [
+        [ww_002, 'Trapper', 'Build one of every cage type (excluding Synth Gorilla)', '20G', 'Bronze'],
+        [ww_001, 'Docile', 'Have 5 tamed creatures in a settlement', '15G', 'Bronze'],
+        [ww_003, 'Instigator', 'Start a spectated arena fight in a settlement', '15G', 'Bronze']
+    ];
 
     return (
         <div className='wasteland'>
             <Link to='/achievement-guides'>
                 <p>Back</p>
             </Link>
-            <section className='sec-intro'>
-                <p>[Replace with Text]</p>
-            </section>
             <section className='main'>
+                <h1>Wasteland Workshop</h1>
                 <Table dat={wasteland} />
                 <Disclaimer />
+            </section>
+            <section classaName='etra-info'>
+                <h1>Helpful Information</h1>
+                <ul>
+                    <li><b>Trapper</b></li>
+                    <ul>
+                        <li>Resources:</li>
+                        <ul>
+                            <li>170 Steel</li>
+                            <li>38 Copper</li>
+                        </ul>
+                    </ul>
+                    <li><b>Docile</b></li>
+                    <li><b>Instigator</b></li>
+                </ul>
             </section>
         </div>
     );
